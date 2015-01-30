004a467a8c40df483471bbd7dce870145e03290a
Guido Contreras Woda <gcontreras@digbang.com>
2015-01-30 14:19:30 -0300

    Improved comments and examples on mapping configuration

--- src/config/mappings.php
+++ src/config/mappings.php
@@ -1,19 +1,32 @@
 <?php
-// Mapping configuration
-// Here you will map your entities, embbedables and
-// Autoloading will be used, and the EntityMapping class
-// will be instantiated through the IoC container, so you
-// can depend on anything the IoC container could resolve.
+/**
+ * Mapping configuration
+ *
+ * Here you will map your entities and embbedables.
+ * Autoloading will be used, and the EntityMapping class
+ * will be instantiated through the IoC container, so you
+ * can depend on anything the IoC container could resolve.
+ */
 return [
-	// -------------------------------------
-	// Mapping of entities.
-	// This maps entities to their EntityMapping mapping class
-	// -------------------------------------
 	'entities' => [
-		'Acme\Mappings\UserMapping'
+		/**
+		 * Array of entity mapping classes.
+		 *
+		 * Example:
+		 *
+		 * App\DataSources\Mappings\UserMapping::class,
+		 * App\DataSources\Mappings\FooMapping::class,
+		 */
 	],
 	'embeddables' => [
-		'Acme\Mappings\DateIntervalMapping'
+		/**
+		 * Array of embeddable (value object) mapping classes.
+		 *
+		 * Example:
+		 *
+		 * App\DataSources\Mappings\EmailMapping::class,
+		 * App\DataSources\Mappings\DateIntervalMapping::class,
+		 */
 	]
 ];
  
