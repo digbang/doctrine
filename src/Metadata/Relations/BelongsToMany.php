7fbd8d0c8c3b7622f1f780320e1d249eee7a3c52
Guido Contreras Woda <gcontreras@digbang.com>
2015-08-06 16:58:52 -0300

    Allow changing the join table in a belongsToMany relation

--- src/Metadata/Relations/BelongsToMany.php
+++ src/Metadata/Relations/BelongsToMany.php
@@ -5,17 +5,48 @@ use Doctrine\ORM\Mapping\Builder\ClassMetadataBuilder;
 class BelongsToMany extends Relation
 {
     /**
+     * @type \Doctrine\ORM\Mapping\Builder\ManyToManyAssociationBuilder
+     */
+    protected $associationBuilder;
+
+    /**
      * @type array
      */
     private $orderColumns = [];
 
-    function __construct(ClassMetadataBuilder $metadataBuilder, $entityName, $relation)
+	/**
+	 * @param ClassMetadataBuilder $metadataBuilder
+	 * @param string               $entityName
+	 * @param string               $relation
+	 */
+    public function __construct(ClassMetadataBuilder $metadataBuilder, $entityName, $relation)
     {
         $this->associationBuilder = $metadataBuilder->createManyToMany(
             $relation, $entityName
         );
     }
 
+	/**
+	 * Change the name of the join table.
+	 *
+	 * @param string $tableName
+	 * @return $this
+	 */
+	public function tableName($tableName)
+	{
+		$this->associationBuilder->setJoinTable($tableName);
+
+		return $this;
+	}
+
+	/**
+	 * Change the default foreign key in the join table.
+	 *
+	 * @param string $foreignKey
+	 * @param string $references
+	 *
+	 * @return $this
+	 */
     public function foreignKeys($foreignKey, $references = 'id')
     {
         $this->associationBuilder->addJoinColumn($foreignKey, $references, false);
@@ -23,6 +54,14 @@ class BelongsToMany extends Relation
         return $this;
     }
 
+	/**
+	 * Change the default inverse key in the join table.
+	 *
+	 * @param string $inverseKey
+	 * @param string $references
+	 *
+	 * @return $this
+	 */
     public function inverseKeys($inverseKey, $references = 'id')
     {
         $this->associationBuilder->addInverseJoinColumn($inverseKey, $references, false);
